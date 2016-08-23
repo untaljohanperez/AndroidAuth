778863baa5ff810767aba0dec28e8994ee52dead
Johan Perez <kedwin@hotmail.com>
2016-08-22 21:28:25 -0500

    first commit

---
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/Controller/ApplicationControllerSingleton.java
@@ -0,0 +1,7 @@
+package com.programacion.movil.estemanp.androidmvcapplication.Controller;
+
+/**
+ * Created by lds on 22/08/2016.
+ */
+public class ApplicationControllerSingleton {
+}
---
+++ app/src/main/java/com/programacion/movil/estemanp/androidmvcapplication/View/homeActivity.java
@@ -0,0 +1,7 @@
+package com.programacion.movil.estemanp.androidmvcapplication.View;
+
+/**
+ * Created by lds on 22/08/2016.
+ */
+public class homeActivity {
+}
